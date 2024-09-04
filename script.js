ab036ceca461c876532f57104b126e18e22d1574
futurDE <abahabrahamochayi@gmail.com>
2024-09-04 07:10:14 +0300

    version 1

---
+++ script.js
@@ -0,0 +1,215 @@
+const english = document.querySelector(".english");
+const turkish = document.querySelector(".turkish");
+const input = document.querySelector("input");
+const continueButton = document.querySelector(".continue-button");
+let enableContinueButton = false;
+
+let random;
+let array = [];
+
+const questionBank = {
+    1: {
+        question: "dog",
+        answer: "köpek",
+    },
+    2: {
+        question: "apple",
+        answer: "elma",
+    },
+    3: {
+        question: "tea",
+        answer: "çay",
+    },
+    4: {
+        question: "too/also",
+        answer: "de",
+    },
+    5: {
+        question: "me too",
+        answer: "ben de",
+    },
+    6: {
+        question: "to do (auxillary verb: e.g to thank)",
+        answer: "etmek",
+    },
+    7: {
+        question: "I do (e.g I give thanks)",
+        answer: "ederim",
+    },
+    8: {
+        question: "at the moment",
+        answer: "anda",
+    },
+    9: {
+        question: "right now/ at the moment",
+        answer: "şu anda",
+    },
+    10: {
+        question: "how",
+        answer: "nasıl",
+    },
+    11: {
+        question: "thing",
+        answer: "şey",
+    },
+    12: {
+        question: "cook",
+        answer: "pişirmek",
+    },
+    13: {
+        question: "to do (literal action)",
+        answer: "yapmak",
+    },
+    14: {
+        question: "to start (e.g to start an activity)",
+        answer: "başlamak",
+    },
+    15: {
+        question: "to start (e.g to start a machinery, device, system)",
+        answer: "çalıştırmak",
+    },
+    16: {
+        question: "let's start (as in activity)",
+        answer: "başlayalım",
+    },
+    17: {
+        question: "I will start (as in activity)",
+        answer: "başlayacağım",
+    },
+    18: {
+        question: "we will start (as in activity)",
+        answer: "başlayacağız",
+    },
+    19: {
+        question: "you will start (as in activity)",
+        answer: "başlayacaksın",
+    },
+    20: {
+        question: "I should start (as in activity)",
+        answer: "başlamalıyım",
+    },
+    21: {
+        question: "you should start (as in activity)",
+        answer: "başlamalısın",
+    },
+    22: {
+        question: "we should start (as in activity)",
+        answer: "başlamalıyız",
+    },
+    23: {
+        question: "they should start (as in activitity)",
+        answer: "başlamalılar",
+    },
+    24: {
+        question: "they will start (as in activity)",
+        answer: "başlayacaklar",
+    },
+    25: {
+        question: "I am cooking",
+        answer: "pişiriyorum",
+    },
+    26: {
+        question: "you are cooking",
+        answer: "pişiriyorsun",
+    },
+    27: {
+        question: "we are cookıng",
+        answer: "pişiriyoruz",
+    },
+    28: {
+        question: "I should cook",
+        answer: "pişirmeliyim",
+    },
+    29: {
+        question: "you should cook",
+        answer: "pişirmelisin",
+    },
+    30: {
+        question: "we should cook",
+        answer: "pişirmeliyiz",
+    },
+    31: {
+        question: "they are cooking",
+        answer: "pişiriyorlar",
+    },
+    32: {
+        question: "they should cook",
+        answer: "pişirmeliler",
+    },
+    33: {
+        question: "let's cook",
+        answer: "pişirelim"
+    },
+    34: {
+        question: "continue",
+        answer: "devam",
+    },
+    35: {
+        question: "great (subjective)",
+        answer: "harika",
+    },
+    36: {
+        question: "perfect (with respect to something)",
+        answer: "mükemmel",
+    },
+    37: {
+        question: "to understand",
+        answer: "anlamak",
+    },
+    38: {
+        questoin: "I understand",
+        answer: "",
+    },
+    39: {
+        question: "",
+        answer: "",
+    },
+};
+
+document.addEventListener("keydown", (event) => {
+    if (event.key == "Enter") {
+        event.preventDefault();
+
+        if (input.value == turkish.textContent) {
+            array.push(questionBank[`${random}`]["question"]);
+            console.log(`This is array: ${array}`);
+            clearInput();
+            displayQuestion();
+        } else {
+            turkish.classList.add("show-turkish");
+            enableContinueButton = true;
+            if (enableContinueButton) {
+                continueButton.addEventListener("click", () => {
+                    turkish.classList.remove("show-turkish");
+                    enableContinueButton = false;
+                    console.log(`This is array after continue button: ${array}`);
+                    clearInput();
+                    displayQuestion();
+                });
+            }
+        }
+    }
+});
+
+function displayQuestion() {
+    random = Math.floor(Math.random() * Object.keys(questionBank).length) + 1;
+    if (array.length != Object.keys(questionBank).length && !array.includes(questionBank[`${random}`]["question"])) {
+        english.textContent = questionBank[`${random}`]["question"];
+        turkish.textContent = questionBank[`${random}`]["answer"];
+    } else if (array.length != Object.keys(questionBank).length && array.includes(questionBank[`${random}`]["question"])) {
+        return displayQuestion();
+    } else {
+        english.style.display = "none";
+        turkish.style.display = "none";
+        continueButton.style.display = "none";
+        let message = document.createElement("p");
+        message.textContent = "Finished!";
+        document.body.appendChild(message);
+    }
+}
+
+function clearInput() {
+    input.value = "";
+}
+
+displayQuestion();
